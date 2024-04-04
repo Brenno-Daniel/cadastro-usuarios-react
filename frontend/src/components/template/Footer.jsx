8f5e3a1383ab15643a7f6aad62c6cce5eb4cc9b7
Brenno Daniel <brenno.daniel.98@gmail.com>
2024-04-04 20:20:30 -0300

    Footer component

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -1,6 +1,13 @@
 import "./Footer.css";
 import React from "react";
 
-const Footer = (props) => <footer className="footer">Rodapé</footer>;
+const Footer = (props) => (
+  <footer className="footer">
+    <span>
+      Desenvolvido com <i className="fa fa-heart text-danger"></i> por{" "}
+      <strong>Brenno</strong>
+    </span>
+  </footer>
+);
 
 export default Footer;
