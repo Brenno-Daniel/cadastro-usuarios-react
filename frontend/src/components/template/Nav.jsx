d19c966a3c8020bb4ee5db424a7f426ff4b0c010
Brenno Daniel <brenno.daniel.98@gmail.com>
2024-04-04 20:32:03 -0300

    Navigation component

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,6 +1,18 @@
 import "./Nav.css";
 import React from "react";
 
-const Nav = (props) => <aside className="menu-area">Navegação</aside>;
+const Nav = (props) => (
+  <aside className="menu-area">
+    <nav className="menu">
+      {/* Refac links to separated components */}
+      <a href="#/">
+        <i className="fa fa-home"></i> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users"></i> Usuários
+      </a>
+    </nav>
+  </aside>
+);
 
 export default Nav;
